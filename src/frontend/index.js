83682b83f3d8f70dfa2bf4533e912d76d444f50a
Constantin Piber <cp.piber@gmail.com>
2021-08-23 19:57:57 +0200

    no-js class, toggle in js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,4 +1,11 @@
-import './style.scss';
 import { i18nDomain } from "../config";
+import './style.scss';
+
+/**
+ * @type {import('jquery')}
+ */
+jQuery($ => {
+    $(document.body).toggleClass('no-js js');
+});
 
 console.log(i18nDomain);
